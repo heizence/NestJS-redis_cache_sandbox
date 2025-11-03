89254c06eb988960079e03d3486d35d7712281f4
doheon <heizence6626@gmail.com>
2025-11-03 11:06:58 +0900

    불필요한 코드 제거

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestMicroserviceOptions } from '@nestjs/common/interfaces/microservices/nest-microservice-options.interface';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
